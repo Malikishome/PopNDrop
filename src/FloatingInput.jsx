095b231dc2c7d826b9bba96b7e8fb567e8fcf1c3
Collin Pierre <Malikishome@gmail.com>
2026-06-28 23:29:07 -0400

    Updated the floatingInputs

--- src/FloatingInput.jsx
+++ src/FloatingInput.jsx
@@ -8,15 +8,17 @@ function FloatingInput({ label, type = "text", name, value, onChange }) {
                 placeholder=" "
                 value={value}
                 onChange={onChange}
-                className="peer w-full border border-gray-400 px-3 pt-6 pb-2 rounded-md outline-none focus:border-amber-500 focus:ring-2 focus:ring-amber-500/10 transition-colors duration-200" />
+                className="peer w-full border border-gray-400 px-3 pt-6 pb-2 rounded-md outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500/10 transition-colors duration-200" />
 
                 <label 
-                    htmlFor={name}
-                    className="absolute left-3 top-4 text-gray-500 transition-all duration-200 ease-in-out
-                    peer-focus:top-1.75 peer-focus:text-[11px] peer-focus:text-amber-500 peer-focus:font-medium
-                    peer-[:not(:placeholder-shown)]:top-1.75 peer-[not(:placeholder-shown)]:text-[11px] peer-[not(:placeholder-shown)]:text-gray-500 peer-[not(:placeholder-shown)]:font-medium">
+                htmlFor={name}
+                    className="absolute left-3 top-4 text-gray-500 transition-all duration-200 ease-in-out pointer-events-none
+                    peer-focus:top-[7px] peer-focus:text-[11px] peer-focus:text-blue-500 peer-focus:font-medium
+                    peer-[:not(:placeholder-shown)]:top-[7px] peer-[:not(:placeholder-shown)]:text-[11px]
+                    peer-[:not(:placeholder-shown)]:text-gray-500 peer-[:not(:placeholder-shown)]:font-medium"
+>
                     {label}
-                </label>
+                    </label>
         </div>
 
     );
